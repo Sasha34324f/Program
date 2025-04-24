4b903653ec014eed023ce39236b5b5efd6f4213b
Sasha34324f <drashpulaleks@gmail.com>
2025-04-24 08:48:41 +0300

    push

--- testGUY.py
+++ testGUY.py
@@ -4,6 +4,6 @@ win = Tk()
 win.title("Pogoda Program")
 win.geometry("500x300")
 win.resizable(False,False) #Заборона зміни розміру вікна
-nap = label(win, text="текст для напису")
-nap.place(relx=0.1, relx=0.4)
+nap = Label(win, text="текст для напису")
+nap.place(relx=0.1, rely=0.4)
 win.mainloop()
