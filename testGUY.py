6baad3ae0293a828209c85099a20f4cf06bef9ba
Sasha34324f <drashpulaleks@gmail.com>
2025-04-25 09:27:56 +0300

    push new

--- testGUY.py
+++ testGUY.py
@@ -5,14 +5,18 @@ win = Tk()
 win.title("Pogoda Program")
 win.geometry("500x300")
 win.resizable(False, False)  # Заборона зміни розміру вікна
-
+def get_into():
+    info1 = ent.get()
+    into2 = combo.get()
+    nap.config(text=into2+info1)
+    print(into2)
 nap = Label(win, font=('Comic Sans MS', 20), text="текст для напису")
 nap.place(relx=0.1, rely=0.1)
 
 ent = Entry(win, font=('Comic Sans MS', 20), width=10)
 ent.place(relx=0.1, rely=0.2)
 
-but = Button(win, text="Click", font=('Comic Sans MS', 20), width=7, height=1)
+but = Button(win, text="Click", font=('Comic Sans MS', 20), width=7, height=1, command=get_into)
 but.place(relx=0.1, rely=0.35)
 
 options = ["Варіант 1", "Варіант 2", "Варіант 3"]
